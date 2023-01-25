28a181007afd5aec1b67d96ec6cab72046f44dac
GabrielEVT <gabrielsoaresevt@gmail.com>
2023-01-25 13:38:48 -0300

    feat: add slideshow animation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        smartSpeed: 1200,
+        items:1,
+        nav: false,
+        dots: true,
+        dotsData: true,
+    });
+});
