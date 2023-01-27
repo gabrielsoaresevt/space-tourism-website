56090cefba7c3a36d52f9cbcf6695059fd1d89e9
GabrielEVT <gabrielsoaresevt@gmail.com>
2023-01-27 11:57:03 -0300

    feat: add technology slideshow

--- js/script.js
+++ js/script.js
@@ -14,4 +14,12 @@ jQuery(document).ready(function($){
         dots: true,
         margin: 50
     });
+    $('.technology__slideshow.owl-carousel').owlCarousel({
+        smartSpeed: 1200,
+        items:1,
+        nav: false,
+        dots: true,
+        dotsData: true,
+        margin: 50
+    });
 });
